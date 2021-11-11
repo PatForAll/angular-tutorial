336ccee363a5c17b0e34344f052554690763e550
PatForAll <siekmeier.p@gmail.com>
2021-11-11 18:35:11 +0100

    implement navigation, dashboard and better hero detail page

--- angular-tutorial/src/app/heroes/heroes.component.ts
+++ angular-tutorial/src/app/heroes/heroes.component.ts
@@ -10,8 +10,6 @@ import {MessageService} from "../message.service";
 })
 export class HeroesComponent implements OnInit {
 
-  selectedHero?: Hero;
-
   heroes: Hero[] = [];
 
   constructor(private heroService: HeroService, private messageService: MessageService) {
@@ -23,11 +21,6 @@ export class HeroesComponent implements OnInit {
 
   getHeroes(): void {
     this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
-  }
-
-  onSelect(hero: Hero) {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`)
   }
 
 }
